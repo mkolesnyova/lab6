5068b8fe7b6a544d4d5c820a2b11b60fb43ac3ce
mkolesnyova <92516020+mkolesnyova@users.noreply.github.com>
2021-11-15 09:35:43 +0300

    Add files via upload

---
+++ lab6(3).cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int a[31], x, n = 0;
+	for (int i = 1; i <= 30; i++)
+	{ 
+		cout << i << "element= ";
+		cin >> a[i];
+		if (a[i] < n) {
+			n = a[i];
+			x = i;
+		}
+		cout << "nomer starshego= " << x;
+
+}
