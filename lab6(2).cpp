e0ff727547a341384addd784b82686ce9b00f5a1
mkolesnyova <92516020+mkolesnyova@users.noreply.github.com>
2021-11-15 09:16:37 +0300

    Add files via upload

---
+++ lab6(2).cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n, s=0, k = 0, betta, z=0, a[20];
+	cout << "chislo betta= ";
+	cin >> betta;
+	cout << "kolichestvo elementov: ";
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		cout << i << " element= ";
+			cin >> a[i];
+			if (a[i] < betta) z = i;
+			if (a[i] > 0) k++;
+			if (a[i] > 3) s += a[i];
+	}
+	if (z > 0) cout << "nomer= " << z << endl;
+	else cout << "net chisel bolshe betta" << endl;
+	cout << "kolichestvo= " << k << endl;
+	cout << "summa= " << s << endl;
+}
