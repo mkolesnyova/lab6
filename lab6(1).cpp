917897800fe63f05efd6a1715ccfa2c59ce59aa1
mkolesnyova <92516020+mkolesnyova@users.noreply.github.com>
2021-11-15 09:19:03 +0300

    Add files via upload

--- lab6(1).cpp
+++ lab6(1).cpp
@@ -2,17 +2,19 @@
 using namespace std;
 int main()
 {
-	double k[10]; /* от к0 до к9 */
+	double a, b, k[10]; /* от к0 до к9 */
 	int n;
 	cout << "vvedite kolichestvo elementov: ";
 	cin >> n;
 	for (int i = 1; i <= n; i++) {
 		cout << i << "element= ";
 		cin >> k[i];
+		a = k[1];
+		b = k[2];
 	}
 	for (int i = 1; i <= n; i++) {
-		if (k[i] > 0) k[i] -= k[1]; 
-		else k[i] -= k[2];
+		if (k[i] > 0) k[i] -=a;
+		else k[i] -= b;
 		cout << k[i] << " ";
 	}
 	return (0);
