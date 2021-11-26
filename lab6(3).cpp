b7762a93740d037588c7c366fe867673ea2c892d
mkolesnyova <92516020+mkolesnyova@users.noreply.github.com>
2021-11-26 12:45:00 +0300

    Update lab6(3).cpp

--- lab6(3).cpp
+++ lab6(3).cpp
@@ -2,15 +2,18 @@
 using namespace std;
 int main()
 {
-	int a[31], x, n = 0;
-	for (int i = 1; i <= 30; i++)
+	int a[30], x, n = 0;
+	for (int i = 0; i < 30; i++)
 	{ 
-		cout << i << "element= ";
+		cout << i+1 << "element= ";
 		cin >> a[i];
-		if (a[i] < n) {
+	}
+	n=a[0];
+	for (i = 0; i < 30; i++)
+	 if (a[i] < n) {
 			n = a[i];
 			x = i;
 		}
-		cout << "nomer starshego= " << x;
+	cout << "nomer starshego= " << x+1;
 
 }
